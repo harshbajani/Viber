ed39ff5b6351c2d5ec80bd7d6f6f287ab87c0d82
harshbajani <harshbajani7@gmail.com>
2025-12-19 16:18:36 +0530

    feat: Implement messages

--- src/generated/prisma/client.ts
+++ src/generated/prisma/client.ts
@@ -29,8 +29,8 @@ export * from "./enums"
  * @example
  * ```
  * const prisma = new PrismaClient()
- * // Fetch zero or more Users
- * const users = await prisma.user.findMany()
+ * // Fetch zero or more Messages
+ * const messages = await prisma.message.findMany()
  * ```
  * 
  * Read more in our [docs](https://pris.ly/d/client).
@@ -40,12 +40,12 @@ export type PrismaClient<LogOpts extends Prisma.LogLevel = never, OmitOpts exten
 export { Prisma }
 
 /**
- * Model User
+ * Model Message
  * 
  */
-export type User = Prisma.UserModel
+export type Message = Prisma.MessageModel
 /**
- * Model Post
+ * Model Fragment
  * 
  */
-export type Post = Prisma.PostModel
+export type Fragment = Prisma.FragmentModel
