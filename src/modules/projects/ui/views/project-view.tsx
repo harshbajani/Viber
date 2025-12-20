9bb648829d9c852a95543c6a233f64c051deb60e
harshbajani <harshbajani7@gmail.com>
2025-12-20 14:20:48 +0530

    feat: Implement fragment view

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -9,6 +9,7 @@ import { MessagesContainer } from "../components/messages-container";
 import { Suspense, useState } from "react";
 import { Fragment } from "@/generated/prisma/client";
 import { ProjectHeader } from "../components/project-header";
+import { FragmentWeb } from "../components/fragment-web";
 
 interface Props {
   projectId: string;
@@ -37,7 +38,9 @@ export const ProjectView = ({ projectId }: Props) => {
           </Suspense>
         </ResizablePanel>
         <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={65} minSize={50}></ResizablePanel>
+        <ResizablePanel defaultSize={65} minSize={50}>
+          {!!activeFragment && <FragmentWeb data={activeFragment} />}
+        </ResizablePanel>
       </ResizablePanelGroup>
     </div>
   );
