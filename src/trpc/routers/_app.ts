e74c622b46762cdab7b9ffac2ad2c7c8a23c8377
harshbajani <harshbajani7@gmail.com>
2025-12-20 10:01:17 +0530

    feat: Implement project module

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
 import { createTRPCRouter } from "../init";
 import { messagesRouter } from "@/modules/messages/server/procedure";
+import { projectsRouter } from "@/modules/projects/server/procedure";
 export const appRouter = createTRPCRouter({
   messages: messagesRouter,
+  projects: projectsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
